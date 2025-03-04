25eb58fb31000e28f65ce9e3decfd0b53428ce7b
Nguyen Phuong Tai <521h0480@student.tdtu.edu.vn>
2025-03-04 14:21:11 +0700

    updated bookmark local

--- logic/lib/src/features/bookmark/use_case/get_all_user_bookmark_courses_use_case.dart
+++ logic/lib/src/features/bookmark/use_case/get_all_user_bookmark_courses_use_case.dart
@@ -10,193 +10,186 @@ class GetAllUserBookmarkCoursesUseCase extends BaseFutureUseCase<
   final BookmarkRepository _bookmarkRepository;
   final LanguageCourseRepository _languageCourseRepository;
   final ExerciseRepository _exerciseRepository;
-  final TrackConnectivityUseCase _trackConnectivityUseCase;
 
   const GetAllUserBookmarkCoursesUseCase(
     this._bookmarkRepository,
     this._languageCourseRepository,
     this._exerciseRepository,
-    this._trackConnectivityUseCase,
   );
 
   @override
   Future<GetAllUserBookmarkCoursesOutput> buildUseCase(
     GetAllUserBookmarkCoursesInput input,
   ) async {
-    final connectivityStream = _trackConnectivityUseCase.buildUseCase(
-      const TrackConnectivityInput(),
-    );
+    final localCategoryCourses = _bookmarkRepository.getLocalCategoryCourse();
+    final localLanguageCourses = _bookmarkRepository.getLocalLanguageCourse();
+    try {
+      final userBookmarkedCourses =
+          await _bookmarkRepository.getUserBookmarkedCourses();
+      if (userBookmarkedCourses.isEmpty) {
+        return const GetAllUserBookmarkCoursesOutput(
+          languageCourses: [],
+          categoryCourses: [],
+        );
+      }
 
-    if ((await connectivityStream.first).isConnected == false) {
-      final localCategoryCourses = _bookmarkRepository.getLocalCategoryCourse();
-      final localLanguageCourses = _bookmarkRepository.getLocalLanguageCourse();
+      final languageCourses = <LanguageCourse>[];
+      final categoryCourses = <CategoryCourse>[];
+      final listOfIdsForLanguageCourses = userBookmarkedCourses
+          .where((element) => element.courseType == CourseType.language)
+          .map((e) => e.courseId)
+          .toList();
 
+      final listOfIdsForCategoryCourses = userBookmarkedCourses
+          .where((element) => element.courseType == CourseType.category)
+          .map((e) => e.courseId)
+          .toList();
+
+      final totalIds = [
+        ...listOfIdsForLanguageCourses,
+        ...listOfIdsForCategoryCourses
+      ];
+
+      if (listOfIdsForLanguageCourses.isNotEmpty) {
+        final languageCoursesData = await _languageCourseRepository
+            .getLanguageCoursesByIds(listOfIdsForLanguageCourses);
+        languageCourses.addAll(languageCoursesData);
+      }
+
+      if (listOfIdsForCategoryCourses.isNotEmpty) {
+        final categoryCoursesData = await _languageCourseRepository
+            .getCategoryCoursesByIds(listOfIdsForCategoryCourses);
+        categoryCourses.addAll(categoryCoursesData);
+      }
+
+      final resultLanguageCourses = <LanguageCourse>[];
+      final resultCategoryCourses = <CategoryCourse>[];
+
+      if (totalIds.isNotEmpty) {
+        final progress = await _exerciseRepository.getLearningProgress(
+          courseIds: totalIds,
+        );
+        final updatedLanguageCourses = languageCourses.map((languageCourse) {
+          final updatedContents =
+              languageCourse.languageCourseLearningContents.map((content) {
+            final contentSize = content.words.length +
+                content.expressions.length +
+                content.idioms.length +
+                content.sentences.length +
+                content.phrasalVerbs.length +
+                content.tenses.length +
+                content.phonetics.length;
+
+            int completedCount = 0;
+
+            progress.flashCardProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == languageCourse.languageCourseId) {
+                completedCount++;
+              }
+            }).toList();
+            progress.quizProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == languageCourse.languageCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            progress.matchingProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == languageCourse.languageCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            progress.pronunciationProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == languageCourse.languageCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            final progressPercentage = (completedCount / contentSize) * 100;
+
+            return content.copyWith(progress: progressPercentage);
+          }).toList();
+
+          return languageCourse.copyWith(
+              languageCourseLearningContents: updatedContents);
+        }).toList();
+        resultLanguageCourses.addAll(updatedLanguageCourses);
+
+        final updatedCategoryCourses = categoryCourses.map((categoryCourse) {
+          final updatedContents =
+              categoryCourse.languageCourseLearningContent.map((content) {
+            final contentSize = content.words.length +
+                content.expressions.length +
+                content.idioms.length +
+                content.sentences.length +
+                content.phrasalVerbs.length +
+                content.tenses.length +
+                content.phonetics.length;
+
+            int completedCount = 0;
+
+            progress.flashCardProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == categoryCourse.categoryCourseId) {
+                completedCount++;
+              }
+            }).toList();
+            progress.quizProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == categoryCourse.categoryCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            progress.matchingProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == categoryCourse.categoryCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            progress.pronunciationProgress.map((e) {
+              if (e.learningContentId ==
+                      content.languageCourseLearningContentId &&
+                  e.courseId == categoryCourse.categoryCourseId) {
+                completedCount++;
+              }
+            }).toList();
+
+            final progressPercentage = (completedCount / contentSize) * 100;
+
+            return content.copyWith(progress: progressPercentage);
+          }).toList();
+
+          return categoryCourse.copyWith(
+            languageCourseLearningContent: updatedContents,
+          );
+        }).toList();
+
+        resultCategoryCourses.addAll(updatedCategoryCourses);
+      }
+
+      return GetAllUserBookmarkCoursesOutput(
+        languageCourses: resultLanguageCourses,
+        categoryCourses: resultCategoryCourses,
+      );
+    } catch (e) {
       return GetAllUserBookmarkCoursesOutput(
         languageCourses: localLanguageCourses,
         categoryCourses: localCategoryCourses,
       );
     }
-
-    final userBookmarkedCourses =
-        await _bookmarkRepository.getUserBookmarkedCourses();
-    if (userBookmarkedCourses.isEmpty) {
-      return const GetAllUserBookmarkCoursesOutput(
-        languageCourses: [],
-        categoryCourses: [],
-      );
-    }
-
-    final languageCourses = <LanguageCourse>[];
-    final categoryCourses = <CategoryCourse>[];
-    final listOfIdsForLanguageCourses = userBookmarkedCourses
-        .where((element) => element.courseType == CourseType.language)
-        .map((e) => e.courseId)
-        .toList();
-
-    final listOfIdsForCategoryCourses = userBookmarkedCourses
-        .where((element) => element.courseType == CourseType.category)
-        .map((e) => e.courseId)
-        .toList();
-
-    final totalIds = [
-      ...listOfIdsForLanguageCourses,
-      ...listOfIdsForCategoryCourses
-    ];
-
-    if (listOfIdsForLanguageCourses.isNotEmpty) {
-      final languageCoursesData = await _languageCourseRepository
-          .getLanguageCoursesByIds(listOfIdsForLanguageCourses);
-      languageCourses.addAll(languageCoursesData);
-    }
-
-    if (listOfIdsForCategoryCourses.isNotEmpty) {
-      final categoryCoursesData = await _languageCourseRepository
-          .getCategoryCoursesByIds(listOfIdsForCategoryCourses);
-      categoryCourses.addAll(categoryCoursesData);
-    }
-
-    final resultLanguageCourses = <LanguageCourse>[];
-    final resultCategoryCourses = <CategoryCourse>[];
-
-    if (totalIds.isNotEmpty) {
-      final progress = await _exerciseRepository.getLearningProgress(
-        courseIds: totalIds,
-      );
-      final updatedLanguageCourses = languageCourses.map((languageCourse) {
-        final updatedContents =
-            languageCourse.languageCourseLearningContents.map((content) {
-          final contentSize = content.words.length +
-              content.expressions.length +
-              content.idioms.length +
-              content.sentences.length +
-              content.phrasalVerbs.length +
-              content.tenses.length +
-              content.phonetics.length;
-
-          int completedCount = 0;
-
-          progress.flashCardProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == languageCourse.languageCourseId) {
-              completedCount++;
-            }
-          }).toList();
-          progress.quizProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == languageCourse.languageCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          progress.matchingProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == languageCourse.languageCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          progress.pronunciationProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == languageCourse.languageCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          final progressPercentage = (completedCount / contentSize) * 100;
-
-          return content.copyWith(progress: progressPercentage);
-        }).toList();
-
-        return languageCourse.copyWith(
-            languageCourseLearningContents: updatedContents);
-      }).toList();
-      resultLanguageCourses.addAll(updatedLanguageCourses);
-
-      final updatedCategoryCourses = categoryCourses.map((categoryCourse) {
-        final updatedContents =
-            categoryCourse.languageCourseLearningContent.map((content) {
-          final contentSize = content.words.length +
-              content.expressions.length +
-              content.idioms.length +
-              content.sentences.length +
-              content.phrasalVerbs.length +
-              content.tenses.length +
-              content.phonetics.length;
-
-          int completedCount = 0;
-
-          progress.flashCardProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == categoryCourse.categoryCourseId) {
-              completedCount++;
-            }
-          }).toList();
-          progress.quizProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == categoryCourse.categoryCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          progress.matchingProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == categoryCourse.categoryCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          progress.pronunciationProgress.map((e) {
-            if (e.learningContentId ==
-                    content.languageCourseLearningContentId &&
-                e.courseId == categoryCourse.categoryCourseId) {
-              completedCount++;
-            }
-          }).toList();
-
-          final progressPercentage = (completedCount / contentSize) * 100;
-
-          return content.copyWith(progress: progressPercentage);
-        }).toList();
-
-        return categoryCourse.copyWith(
-          languageCourseLearningContent: updatedContents,
-        );
-      }).toList();
-
-      resultCategoryCourses.addAll(updatedCategoryCourses);
-    }
-
-    return GetAllUserBookmarkCoursesOutput(
-      languageCourses: resultLanguageCourses,
-      categoryCourses: resultCategoryCourses,
-    );
   }
 }
 
